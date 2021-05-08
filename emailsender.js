d7b5d1f53dde6b8d272f0ab1008c827bdee36c77
Arthur Britto <arthurbrittos@gmail.com>
2021-05-08 13:59:46 +0000

    Create emailsender.js

---
+++ emailsender.js
@@ -0,0 +1,30 @@
+
+document.querySelector(".contact-form").addEventListener("submit", submitForm);
+
+function submitForm(e){
+    e.preventDefault();
+
+    let name = document.querySelector(".name").value;
+    let email = document.querySelector(".email").value;
+    let message = document.querySelector(".message").value;
+
+    sendEmail(name, email, message);
+    document.querySelectorAll.reset();
+}
+
+
+
+function sendEmail(name, email, message) {
+    Email.send({
+        Host: "smtp.gmail.com",
+        Username: "emailmanagementarthur@gmail.com",
+        Password: "YSLJ7QiQ7bn9dAu",
+        To: 'arthurbrittos@gmail.com',
+        From: "emailmanagementarthur@gmail.com",
+        Subject: `${name} send you a message from your Website`,
+        Body: `Name: ${name} <br/> Email: ${email} <br/>
+        Message: ${message}`,
+    }).then(
+        message => alert("Message sucessfully sent")
+    );
+}
